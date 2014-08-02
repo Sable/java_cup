6abbd249821dad02ebe3e9b1ad01ab747227955d
petter <petter>
2014-08-02 15:38:57 +0000

    finetuned C Parser

--- testgrammars/c/makeTest.sh
+++ testgrammars/c/makeTest.sh
@@ -3,4 +3,5 @@ rm *.class
 jflex c.jflex
 java -jar ../../dist/java-cup-11b.jar -locations -interface -parser Parser -xmlactions c.cup
 javac -cp ../../dist/java-cup-11b-runtime.jar:. *.java
-java -cp ../../dist/java-cup-11b-runtime.jar:. Parser input.c test.xml
+java -cp ../../dist/java-cup-11b-runtime.jar:. Parser input.c simple.xml
+java -cp ../../dist/java-cup-11b-runtime.jar:. Parser complicated.c complicated.xml
